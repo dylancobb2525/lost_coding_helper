b684b52ad9939ab6bf17bfd422113cc08541677a
dylancobb2525 <dylancobb2525@gmail.com>
2026-02-26 14:09:58 -0500

    added maven dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.lost_coding_helper {
     requires javafx.controls;
     requires javafx.fxml;
+    requires json.simple;
 
     opens com.lost_coding_helper to javafx.fxml;
     exports com.lost_coding_helper;
