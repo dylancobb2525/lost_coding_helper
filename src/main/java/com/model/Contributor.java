9513f63955a204123f9b1c0a044f0cdaaf5931b6
Real-Blakelf <real.blakefranks@gmail.com>
2026-02-27 23:03:04 -0500

    Update Contributor.java

---
+++ src/main/java/com/model/Contributor.java
@@ -0,0 +1,36 @@
+package com.model;
+
+public class Contributor extends RegisteredUser {
+
+    public Contributor() {
+        
+    }
+
+    public void addQuestion(Question question) {
+        
+    }
+
+    public void editQuestion(Question question) {
+        
+    }
+
+    public void deleteQuestion(Question question) {
+        
+    }
+
+    public void addTestCases(Question question, TestCase[] testCases) {
+        
+    }
+
+    public void assignTopics(Question question, Topic[] topics) {
+        
+    }
+
+    public void addHints(Question question, String[] hints) {
+        
+    }
+
+    public void setComplexity(Question question, String timeComplexity, String spaceComplexity) {
+         
+    }
+}
