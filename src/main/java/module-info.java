60c9a2984fa32606ab9a48616c93fb4ac0e85153
dylancobb2525 <dylancobb2525@gmail.com>
2026-03-24 14:13:27 -0400

    added test folder

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.lost_coding_helper {
     requires javafx.controls;
     requires javafx.fxml;
     requires json.simple;
+    requires junit;
 
     opens com.lost_coding_helper to javafx.fxml;
     exports com.lost_coding_helper;
